99c0089c8724e9a3d6860e26e12dd0698c5e1ff6
Garrett <Garrett.Bodley@gmail.com>
2021-01-07 18:17:36 -0500

    Finished Lab

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    if comment_params[:user_id].empty?
+      user = User.new(comment_params[:user_attributes])
+      user.comments << comment
+    else 
+      User.find(comment_params[:user_id]).comments << comment
+    end
+
     redirect_to comment.post
   end
 
@@ -10,4 +17,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def users_attributes(users)
+    users.values.each do |u|
+      
+    end
+  end
+
 end
